d32aeae37bf559f4a9665105eda9f9a3450b1e90
Abdulaziz Jamaleddin <abduljeddin@gmail.com>
2023-11-13 11:28:20 -0500

    initial

---
+++ SwiftUIStoryTemplate/Chapter1/Chapter1View.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct Chapter1View: View {
+    var body: some View {
+        ScrollView{
+            Circle()
+            Circle()
+
+            Circle()
+
+            Circle()
+
+            Circle()
+
+        }
+    }
+}
+
+#Preview {
+    Chapter1View()
+}
---
+++ SwiftUIStoryTemplate/SwiftUIStoryTemplateApp.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUIStoryTemplateApp.swift
+//  SwiftUIStoryTemplate
+//
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStoryTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
